6e0b06d7ecde064a366b1546dfd365d771add252
Leachim <32847549+Licheam@users.noreply.github.com>
2023-05-02 19:17:05 +0800

    modified:   Formula/tlsdate.rb

--- Formula/tlsdate.rb
+++ Formula/tlsdate.rb
@@ -3,8 +3,8 @@ class Tlsdate < Formula
   url 'https://github.com/ioerror/tlsdate/archive/refs/tags/tlsdate-0.0.13.tar.gz'
   # This hash will never be correct until we put a tagged version into master
   # update accordingly until we do a proper release that supports OS X
-  sha256 '90efdff87504b5159cb6a3eefa9ddd43723c073d49c4b3febba9e48fc1292bf9'
-  version '0.0.13'
+  sha256 '61aaee6e109484965d38f9ffd08f0ea4cbe967ba61027bc29d7bbdc925d21417'
+  version 'db811efad1d655e5aad763d0871aa4e97fcbfcfc'
 
   depends_on 'autoconf' => :build
   depends_on 'automake' => :build
@@ -13,14 +13,10 @@ class Tlsdate < Formula
   depends_on 'pkg-config' => :build
   depends_on 'openssl@1.1'
 
-  patch do
-    url "https://github.com/ioerror/tlsdate/compare/tlsdate-0.0.13...akiernan:tlsdate:master.patch?full_index=1"
-    sha256 "ed9fef28aec4ba4e21574258be391958c5c2f3b088a529df6f7e45837c6db5d4"
-  end
-
   def install
     system "./autogen.sh"
-    system "./configure", "--prefix=#{prefix}"
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
     system "make"
     system "make install"
   end
